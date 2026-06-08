50ca48e87a824417f2f216746a811b518a5a3648
Jeff DeCola (tk3) <jeffdecola@gmail.com>
2026-06-08 11:56:04 -0400

    starting to update perceptron with neuron pacakge

--- perceptron/perceptron-simple-example/README.md
+++ perceptron/perceptron-simple-example/README.md
@@ -12,16 +12,48 @@ Table of Contents
 
 Documentation and Reference
 
-* [artificial intelligence](https://github.com/JeffDeCola/my-cheat-sheets/tree/master/software/development/software-architectures/artificial-intelligence/artificial-intelligence-cheat-sheet)
-* [neural networks](https://github.com/JeffDeCola/my-cheat-sheets/tree/master/software/development/software-architectures/artificial-intelligence/artificial-intelligence-cheat-sheet/neural-networks.md)
+* [artificial intelligence overview](https://github.com/JeffDeCola/my-cheat-sheets/tree/master/software/development/software-architectures/artificial-intelligence/ai-fundamentals/artificial-intelligence-overview-cheat-sheet#artificial-intelligence-overview-cheat-sheet)
+* [neural networks](https://github.com/JeffDeCola/my-cheat-sheets/tree/master/software/development/software-architectures/artificial-intelligence/ai-fundamentals/neural-networks-cheat-sheet#neural-networks-cheat-sheet)
+  * [mathematical model of a neuron](https://github.com/JeffDeCola/my-cheat-sheets/tree/master/software/development/software-architectures/artificial-intelligence/ai-fundamentals/neural-networks-cheat-sheet#mathematical-model-of-a-neuron)
 
 ## OVERVIEW
 
-The perceptron (P) is a simple algorithm that can be used to classify data
-into two categories.
-It is a single layer neural network that takes
-in a set of inputs, weights them, and then outputs a 1 or 0 based
-on the weighted sum of the inputs.
+A perceptron is a simple single-layer neural network that takes N inputs,
+applies a learned weight to each, sums them with a bias,
+and produces a single output of 1 or 0 via a step activation function.
 
-I have a cheat sheet on the
-[mathematical model of a neuron](https://github.com/JeffDeCola/my-cheat-sheets/tree/master/software/development/software-architectures/artificial-intelligence/artificial-intelligence-cheat-sheet/neural-networks.md#mathematical-model-of-a-neuron)
+This is an illustration of a 2 input perceptron.
+
+<p align="center">
+    <img src="https://raw.githubusercontent.com/JeffDeCola/my-cheat-sheets/refs/heads/master/docs/pics/software/development/neural-networks-perceptron.svg"
+    alt="perceptron">
+</p>
+
+## MODEL A NEURON IN GO
+
+First, we must model a neuron in go. From my cheat sheet, we made a model of a neuron.
+
+<p align="center">
+    <img src="https://raw.githubusercontent.com/JeffDeCola/my-cheat-sheets/refs/heads/master/docs/pics/software/development/neural-networks-mathematical-model-of-a-neuron.svg"
+    alt="perceptron">
+</p>
+
+Let's create a structure to hold weights and bias,
+
+type Perceptron struct {
+    weights []float64
+    bias    float64
+}
+
+Then we can create
+
+## AND GATE EXAMPLE
+
+For this example, we're going to model an AND gate (2 input and 1 output).
+
+| INPUT 1 | INPUT 2 | OUTPUT |
+|---------|---------|--------|
+| 0       | 0       | 0      |
+| 1       | 0       | 0      |
+| 0       | 1       | 0      |
+| 1       | 1       | 1      |
--- perceptron/perceptron-simple-example/main.go
+++ perceptron/perceptron-simple-example/main.go
@@ -1,81 +1,78 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "time"
+	"fmt"
+	"perceptron-simple-example/neuron"
 )
 
-// Perceptron struct
-type Perceptron struct {
-    weights []float64
-    bias    float64
+// PUBLIC - Configuration
+type PerceptronConfiguration struct {
+	n int // number of input nodes
 }
 
-// NewPerceptron creates a new Perceptron with random weights and bias
-func NewPerceptron(inputSize int) *Perceptron {
-    rand.Seed(time.Now().UnixNano())
-    weights := make([]float64, inputSize)
-    for i := range weights {
-        weights[i] = rand.Float64()*2 - 1 // Initialize weights between -1 and 1
-    }
-    bias := rand.Float64()*2 - 1 // Initialize bias between -1 and 1
-    return &Perceptron{weights: weights, bias: bias}
+// PRIVATE - Perceptron
+type perceptron struct {
+	x []float64      // 1 of more input nodes
+	o *neuron.Neuron // 1 output node
 }
 
-// Activation function (step function)
-func (p *Perceptron) Activation(sum float64) float64 {
-    if sum >= 0 {
-        return 1
-    }
-    return 0
+// Make Perceptron
+func (p PerceptronConfiguration) makePerceptron() *perceptron {
+
+	// Build
+	in := make([]float64, p.n)
+	on := neuron.MakeNeuron()
+
+	// Assemble
+	per := perceptron{
+		x: in,
+		o: on,
+	}
+
+	// RETURN
+	return &per
 }
 
-// Predict makes a prediction based on input features
-func (p *Perceptron) Predict(inputs []float64) float64 {
-    sum := p.bias
-    for i, input := range inputs {
-        sum += input * p.weights[i]
-    }
-    return p.Activation(sum)
+// RUN
+func (p *perceptron) predict(input []float64) {
+
+	p.x[0] = input[0]
+	p.x[1] = input[1]
+
+	p.o.B = 1
+
 }
 
-// Train trains the Perceptron using the given dataset
-func (p *Perceptron) Train(dataset [][]float64, labels []float64, epochs int, learningRate float64) {
-    for epoch := 0; epoch < epochs; epoch++ {
-        for i, inputs := range dataset {
-            prediction := p.Predict(inputs)
-            error := labels[i] - prediction
-            for j := range p.weights {
-                p.weights[j] += learningRate * error * inputs[j]
-            }
-            p.bias += learningRate * error
-        }
-    }
+// Print current state of Perceptron
+func (p *perceptron) printPerceptron() {
+
+	fmt.Println("INPUT NODES")
+	for i, in := range p.x {
+		fmt.Printf("  x[%d] %v\n", i, in)
+	}
+	fmt.Println("OUTPUT NODE")
+	fmt.Printf("  o    %v\n", p.o.B)
+
 }
 
 func main() {
 
-    // Example dataset (AND logic gate)
-    dataset := [][]float64{
-        {0, 0},
-        {0, 1},
-        {1, 0},
-        {1, 1},
-    }
-    labels := []float64{0, 0, 0, 1}
+	// Configuration
+	cfg := PerceptronConfiguration{
+		n: 2,
+	}
 
-    // Create a Perceptron
-    perceptron := NewPerceptron(2)
+	// Make perceptron with default values
+	p := cfg.makePerceptron()
 
-    // Train the Perceptron
-    epochs := 100
-    learningRate := 0.1
-    perceptron.Train(dataset, labels, epochs, learningRate)
+	// Print Perceptron
+	p.printPerceptron()
 
-    // Test the Perceptron
-    for _, inputs := range dataset {
-        prediction := perceptron.Predict(inputs)
-        fmt.Printf("Inputs: %v, Prediction: %v\n", inputs, prediction)
-    }
+	// predict
+	input := []float64{1, 0}
+	p.predict(input)
+
+	// Print Perceptron
+	p.printPerceptron()
+
 }
